d54fa22643ee938cef3606749f1169411b33fcff
Precious <precious.akpan2000@gmail.com>
2021-07-06 19:16:21 +0100

    Updated robohash set

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ name, email, id }) => {
   return (
     <div className="bg-light-green tc dib br3 pa3 ma2 grow shadow-5 bw2 ">
-      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
+      <img alt="robots" src={`https://robohash.org/${id}?set=set2`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
